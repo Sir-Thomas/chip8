c877eed9780432ea0d06dc7bd084e12bcb153a19
Sir-Thomas <tp8153@gmail.com>
2026-09-10 20:19:41 -0500

    initial commit
    bad graphics
    no input
    no audio

---
+++ src/main.rs
@@ -0,0 +1,398 @@
+const START_ADDRESS: u16 = 0x200;
+const FONTSET_START_ADDRESS: u16 = 0x50;
+
+struct Chip8 {
+    registers: [u8; 16],
+    memory: [u8; 4096],
+    index: u16,
+    program_counter: u16,
+    stack: [u16; 16],
+    stack_pointer: u8,
+    delay_timer: u8,
+    sound_timer: u8,
+    keypad: [bool; 16],
+    video: [u32; 64 * 32],
+}
+
+impl Chip8 {
+    fn new() -> Self {
+        Chip8 {
+            registers: [0; 16],
+            memory: [0; 4096],
+            index: 0,
+            program_counter: START_ADDRESS,
+            stack: [0; 16],
+            stack_pointer: 0,
+            delay_timer: 0,
+            sound_timer: 0,
+            keypad: [false; 16],
+            video: [0; 64 * 32],
+        }
+    }
+
+    fn load_fontset(&mut self) {
+        let fontset = [
+            0xF0, 0x90, 0x90, 0x90, 0xF0, // 0
+            0x20, 0x60, 0x20, 0x20, 0x70, // 1
+            0xF0, 0x10, 0xF0, 0x80, 0xF0, // 2
+            0xF0, 0x10, 0xF0, 0x10, 0xF0, // 3
+            0x90, 0x90, 0xF0, 0x10, 0x10, // 4
+            0xF0, 0x80, 0xF0, 0x10, 0xF0, // 5
+            0xF0, 0x80, 0xF0, 0x90, 0xF0, // 6
+            0xF0, 0x10, 0x20, 0x40, 0x40, // 7
+            0xF0, 0x90, 0xF0, 0x90, 0xF0, // 8
+            0xF0, 0x90, 0xF0, 0x10, 0xF0, // 9
+            0xF0, 0x90, 0xF0, 0x90, 0x90, // A
+            0xE0, 0x90, 0xE0, 0x90, 0xE0, // B
+            0xF0, 0x80, 0x80, 0x80, 0xF0, // C
+            0xE0, 0x90, 0x90, 0x90, 0xE0, // D
+            0xF0, 0x80, 0xF0, 0x80, 0xF0, // E
+            0xF0, 0x80, 0xF0, 0x80, 0x80  // F
+        ];
+
+        for (i, &byte) in fontset.iter().enumerate() {
+            self.memory[FONTSET_START_ADDRESS as usize + i] = byte;
+        }
+    }
+
+    fn load_rom(&mut self, filename: String) {
+        let file = std::fs::read(filename).expect("Failed to open ROM file");
+
+        for (i, &byte) in file.iter().enumerate() {
+            self.memory[START_ADDRESS as usize + i] = byte;
+        }
+    }
+
+    fn process_opcode(&mut self, opcode: u16) {
+        let nibble1 = ((opcode & 0xF000) >> 12) as u8;
+        let address = opcode & 0x0FFF;
+        let vx = ((opcode & 0x0F00) >> 8) as u8;
+        let vy = ((opcode & 0x00F0) >> 4) as u8;
+        let byte = (opcode & 0x00FF) as u8;
+        let nibble2 = (opcode & 0x000F) as u8;
+        match nibble1 {
+            0x0 => {
+                match opcode {
+                    // CLS: Clear the display
+                    0x00e0 => self.video = [0; 64 * 32],
+                    // RET: Return from a subroutine
+                    0x00ee => {
+                        self.stack_pointer -= 1;
+                        self.program_counter = self.stack[self.stack_pointer as usize];
+                    },
+                    _ => {} // TODO: error on invalid value
+                }
+            }
+            0x1 => self.op_1nnn(address),
+            0x2 => self.op_2nnn(address),
+            0x3 => self.op_3xkk(vx, byte),
+            0x4 => self.op_4xkk(vx, byte),
+            0x5 => self.op_5xy0(vx, vy),
+            0x6 => self.op_6xkk(vx, byte),
+            0x7 => self.op_7xkk(vx, byte),
+            0x8 => self.op_8xyn(vx, vy, nibble2),
+            0x9 => self.op_9xy0(vx, vy),
+            0xA => self.op_annn(address),
+            0xB => self.op_bnnn(address),
+            0xC => self.op_cxkk(vx, byte),
+            0xD => self.op_dxyn(vx, vy, nibble2),
+            0xE => self.op_exkk(vx, byte),
+            0xF => self.op_fxkk(vx, byte),
+            _ => {} //TODO: error on invalid value
+        }
+    }
+
+    // JP addr: Jump to location nnn
+    fn op_1nnn(&mut self, address: u16) {
+        self.program_counter = address;
+    }
+
+    // CALL addr: Call subroutine at address nnn
+    fn op_2nnn(&mut self, address: u16) {
+        self.stack[self.stack_pointer as usize] = self.program_counter;
+        self.stack_pointer += 1;
+        self.program_counter = address;
+    }
+
+    // SE Vx, byte
+    fn op_3xkk(&mut self, vx: u8, byte: u8) {
+        if self.registers[vx as usize] == byte {
+            self.program_counter += 2;
+        }
+    }
+
+    // SNE Vx, byte
+    fn op_4xkk(&mut self, vx: u8, byte: u8) {
+        if self.registers[vx as usize] != byte {
+            self.program_counter += 2;
+        }
+    }
+
+    // SE Vx, Vy
+    fn op_5xy0(&mut self, vx: u8, vy: u8) {
+        if self.registers[vx as usize] == self.registers[vy as usize] {
+            self.program_counter += 2;
+        }
+    }
+
+    // LD Vx, byte
+    fn op_6xkk(&mut self, vx: u8, byte: u8) {
+        self.registers[vx as usize] = byte;
+    }
+
+    // ADD Vx, byte
+    fn op_7xkk(&mut self, vx: u8, byte: u8) {
+        self.registers[vx as usize] = (self.registers[vx as usize] as usize + byte as usize) as u8;
+    }
+
+    fn op_8xyn(&mut self, vx: u8, vy: u8, nibble: u8) {
+        match nibble {
+            0x0 => self.registers[vx as usize] = self.registers[vy as usize],
+            0x1 => self.registers[vx as usize] |= self.registers[vy as usize],
+            0x2 => self.registers[vx as usize] &= self.registers[vy as usize],
+            0x3 => self.registers[vx as usize] ^= self.registers[vy as usize],
+            0x4 => self.sum(vx, vy),
+            0x5 => self.diff(vx, vy),
+            0x6 => self.shr(vx),
+            0x7 => self.subn(vx, vy),
+            0xE => self.shl(vx),
+            _ => {},
+        }
+    }
+
+    fn sum(&mut self, vx: u8, vy: u8) {
+        let sum = self.registers[vx as usize] as usize + self.registers[vy as usize] as usize;
+        if sum > 255 {
+            self.registers[0xF] = 1;
+        } else {
+            self.registers[0xF] = 0;
+        }
+
+        self.registers[vx as usize] = sum as u8 & 0xFF;
+    }
+
+    fn diff(&mut self, vx: u8, vy: u8) {
+        if self.registers[vx as usize] > self.registers[vy as usize] {
+            self.registers[0xF] = 1;
+        } else {
+            self.registers[0xF] = 0;
+        }
+
+        self.registers[vx as usize] = (self.registers[vx as usize] as isize - self.registers[vy as usize] as isize) as u8;
+    }
+
+    fn shr(&mut self, vx: u8) {
+        self.registers[0xF] = self.registers[vx as usize] & 0x1;
+        self.registers[vx as usize] >>= 1;
+    }
+
+    fn subn(&mut self, vx: u8, vy: u8) {
+        if self.registers[vy as usize] > self.registers[vx as usize] {
+            self.registers[0xF] = 1;
+        } else {
+            self.registers[0xF] = 0;
+        }
+
+        self.registers[vx as usize] = self.registers[vy as usize] - self.registers[vx as usize];
+    }
+
+    fn shl(&mut self, vx: u8) {
+        self.registers[0xF] = (self.registers[vx as usize] & 0x80) >> 7;
+
+        self.registers[vx as usize] <<= 1;
+    }
+
+    fn op_9xy0(&mut self, vx: u8, vy: u8) {
+        if self.registers[vx as usize] != self.registers[vy as usize] {
+            self.program_counter += 2;
+        }
+    }
+
+    fn op_annn(&mut self, address: u16) {
+        self.index = address;
+    }
+
+    fn op_bnnn(&mut self, address: u16) {
+        self.program_counter = self.registers[0] as u16 + address;
+    }
+
+    fn op_cxkk(&mut self, vx: u8, byte: u8) {
+        self.registers[vx as usize] = rand::random::<u8>() & byte;
+    }
+
+    fn op_dxyn(&mut self, vx: u8, vy: u8, height: u8) {
+        let x_pos = self.registers[vx as usize] as usize % 64; // TODO: make this a const
+        let y_pos = self.registers[vy as usize] as usize % 32;
+
+        self.registers[0xF] = 0;
+
+        for row in 0..height as usize {
+            let byte = self.memory[self.index as usize + row as usize];
+            for col in 0..8 {
+                let sprite_pixel = byte & (0x80 >> col);
+                if sprite_pixel != 0x0 {
+                    let x = (x_pos + col) % 64;
+                    let y = (y_pos + row) % 32;
+                    let screen_pixel = self.video[(y * 64 + x) as usize];
+                    if screen_pixel == 0xFFFFFFFF {
+                        self.registers[0xF] = 1; // TODO: Check if this is accurate
+                    } 
+                    self.video[((y_pos + row) * 64 + x_pos + col) as usize] = screen_pixel ^ 0xFFFFFFFF;
+                }
+            }
+        }
+    }
+
+    fn op_exkk(&mut self, vx: u8, byte: u8) {
+        let key = self.registers[vx as usize];
+
+        let pressed = match byte {
+            0x9E => true,
+            0xA1 => false,
+            _ => true, // TODO: error on invalid value
+        };
+
+        if self.keypad[key as usize] == pressed {
+            self.program_counter += 2;
+        }
+    }
+
+    fn op_fxkk(&mut self, vx: u8, byte: u8) {
+        match byte {
+            0x07 => self.registers[vx as usize] = self.delay_timer,
+            0x0A => self.wait_for_keypress(vx),
+            0x15 => self.delay_timer = self.registers[vx as usize],
+            0x18 => self.sound_timer = self.registers[vx as usize],
+            0x1E => self.index += self.registers[vx as usize] as u16,
+            0x29 => self.load_font(vx),
+            0x33 => self.binary_coded_decimal(vx),
+            0x55 => self.store_registers(vx),
+            0x65 => self.load_registers(vx),
+            _ => {} // TODO: error on invalid value
+        }
+    }
+
+    fn wait_for_keypress(&mut self, vx: u8) {
+        for i in 0..16 {
+            if self.keypad[i as usize] {
+                self.registers[vx as usize] = i;
+                return;
+            }
+        }
+        self.program_counter -= 2;
+    }
+
+    fn load_font(&mut self, vx: u8) {
+        self.index = FONTSET_START_ADDRESS + 5 * self.registers[vx as usize] as u16;
+    }
+
+    fn binary_coded_decimal(&mut self, vx: u8) {
+        let value = self.registers[vx as usize];
+        self.memory[self.index as usize + 2] = value % 10;
+        let value = value / 10;
+        self.memory[self.index as usize + 1] = value % 10;
+        let value = value / 10;
+        self.memory[self.index as usize] = value % 10;
+    }
+
+    fn store_registers(&mut self, vx: u8) {
+        for i in 0..=vx as usize {
+            self.memory[self.index as usize + i] = self.registers[i];
+        }
+    }
+
+    fn load_registers(&mut self, vx: u8) {
+        for i in 0..=vx as usize {
+            self.registers[i] = self.memory[self.index as usize + i];
+        }
+    }
+
+    fn cycle(&mut self) {
+        let msb = self.memory[self.program_counter as usize] as u16;
+        let lsb = self.memory[self.program_counter as usize + 1] as u16;
+        let opcode = (msb << 8) + lsb;
+
+        println!("PC: {:#06X} | Opcode: {:#06X} | Index: {:#06X}", self.program_counter, opcode, self.index);
+        for register in 0..16 {
+            println!("Register {:X}: {:#06X}", register, self.registers[register]);
+        }
+
+        self.program_counter += 2;
+
+        self.process_opcode(opcode);
+
+        if self.delay_timer > 0 {
+            self.delay_timer -= 1;
+        }
+
+        if self.sound_timer > 0 {
+            self.sound_timer -= 1;
+        }
+    }
+
+    fn process_input(&mut self) -> bool {
+        false
+    }
+}
+
+fn main() {
+    print!("\x1B[2J\x1B[H");
+    let mut args = env::args();
+    if args.len() < 4 {
+        println!("Usage: {} <Scale> <Delay> <ROM>", args.next().unwrap());
+        // TODO: exit on incorrect args
+    } else {
+        args.next();
+    }
+
+    let _scale: usize = args.next().and_then(|s| s.parse().ok()).unwrap_or(1);
+    let delay: i64 = args.next().and_then(|s| s.parse().ok()).unwrap_or(4);
+    let rom: String = args.next().unwrap_or_else(|| String::from("roms/particle_demo.ch8"));
+
+    let mut chip8 = Chip8::new();
+
+    chip8.load_fontset();
+    chip8.load_rom(rom);
+
+    let mut quit = false;
+
+    let mut last_cycle_time = jiff::Timestamp::now();
+
+    let display = true;
+
+    while !quit {
+        quit = chip8.process_input();
+        let current_time = jiff::Timestamp::now();
+        let delta = current_time - last_cycle_time;
+        if delta.get_milliseconds() > delay {
+            last_cycle_time = current_time;
+            chip8.cycle();
+            if display {
+                display_video(chip8.video);
+                play_audio();
+                update_keyboard();
+            } else {
+                let mut input = String::new();
+                std::io::stdin().read_line(&mut input).expect("Failed to read line");
+            }
+        }
+    }
+}
+
+fn display_video(video: [u32; 64 * 32]) {
+    print!("\x1B[2J\x1B[H");
+    //print!("\x1B[H");
+    
+    for row in video.chunks_exact(64) {
+        for &pixel in row {
+            print!("{}", if pixel == 0xFFFFFFFF { "█" } else { " " });
+        }
+        println!();
+    }
+}
+
+fn play_audio() {
+}
+
+fn update_keyboard() {
+}
